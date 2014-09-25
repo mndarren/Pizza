140d7739a33642a8e35ae2a3451d04d1429a680d
leonluocheng <leon.luocheng@gmail.com>
2014-09-25 16:10:32 -0500

    Test

--- ConsoleApplication1/Source.cpp
+++ ConsoleApplication1/Source.cpp
@@ -8,6 +8,6 @@ int main(){
 	cin >> god;
 	cout << "This is the test for pizza project!" << endl;
 	cout << "And this is another line" << endl;
-	
+	cout << "This is another test" << endl;
 	return 0;
 }
