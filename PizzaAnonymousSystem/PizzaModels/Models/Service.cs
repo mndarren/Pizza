d8fd1b26cdf3967f0bdd8825491fd1355de6da21
zhaoxiemn <qddarrenxie@gmail.com>
2014-11-04 22:50:51 -0600

    up

--- PizzaAnonymousSystem/PizzaModels/Models/Service.cs
+++ PizzaAnonymousSystem/PizzaModels/Models/Service.cs
@@ -7,5 +7,13 @@ namespace PizzaModels.Models
 {
     public class Service
     {
+        private int serviceCode;
+        private string serviceName;
+        private float serviceFee;
+
+        public int GetServiceCode( ){return serviceCode;}
+        public string GetServiceName() { return serviceNmae; }
+        public float GetServiceFee() { return serviceFee; }
+        public int Set
     }
 }
