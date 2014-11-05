5399b4cccfa841978f2b3b8aebb88764b5a89595
zhaoxiemn <qddarrenxie@gmail.com>
2014-11-05 09:55:35 -0600

    update service

--- PizzaAnonymousSystem/PizzaModels/Models/Service.cs
+++ PizzaAnonymousSystem/PizzaModels/Models/Service.cs
@@ -12,9 +12,13 @@ namespace PizzaModels.Models
         private float _serviceFee;
 
         public Service ()
-	{
-
-	}
+	   {
+          _serviceCode = 0; _serviceName = null; _serviceFee = 0.0;
+	   }
+        public Service(int code,string name,float fee)
+        {
+            _serviceCode = code; _serviceName = name; _serviceFee = fee;
+        }
         public int ServiceCode { get {return _serviceCode;} set {_serviceCode = value;}}
         public string ServiceName { get { return _serviceName; } set { _serviceName = value; } }
         public float ServiceFee { get { return _serviceFee; } set { _serviceFee = value; } }
