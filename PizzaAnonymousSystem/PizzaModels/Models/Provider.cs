267379b15982bb0e1cdd138c32e7bb31956ed322
zhaoxiemn <qddarrenxie@gmail.com>
2014-11-05 11:50:09 -0600

    update provider class

--- PizzaAnonymousSystem/PizzaModels/Models/Provider.cs
+++ PizzaAnonymousSystem/PizzaModels/Models/Provider.cs
@@ -7,9 +7,15 @@ namespace PizzaModels.Models
 {
     public class Provider
     {
+        private long _bankAccount;
         public Provider()
         {
-
+            _bankAccount = 0;
         }
+        public Provider(long num)
+        {
+            _bankAccount = num;
+        }
+        public BankAccount{get {return _bankAccount;} set {_bankAccount = value;}}
     }
 }
