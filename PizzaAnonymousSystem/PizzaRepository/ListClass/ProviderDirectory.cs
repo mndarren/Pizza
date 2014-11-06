d87be613c29d9e45f83b28dfd6c4174ae7e3e883
zhaoxiemn <qddarrenxie@gmail.com>
2014-11-05 23:28:37 -0600

    changed

--- PizzaAnonymousSystem/PizzaRepository/ListClass/ProviderDirectory.cs
+++ PizzaAnonymousSystem/PizzaRepository/ListClass/ProviderDirectory.cs
@@ -11,6 +11,25 @@ namespace PizzaRepository.ListClass
     public class ProviderDirectory : IProviderDirectory
     {
         private List<Service> serviceItems;
+        private const int OK = 1;
+        public ProviderDirectory(){}
+        public ProviderDirectory()
+        {
 
+        }
+        public List<Service> ServiceItems{get {return serviceItems;}}
+        public int AddServiceItem(Service newItem)
+        {
+            serviceItems.Add(newItem);
+            return OK;
+        }
+        public int deleteServiceItem(int serviceID)
+        { 
+           if(serviceItems._serviceID == serviceID)
+           serviceItems.Remove(serviceItem);
+            return OK;
+        }
+        public List get
+        public int updateService()
     }
 }
