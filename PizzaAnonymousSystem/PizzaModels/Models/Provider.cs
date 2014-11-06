ab6a6c984e0078e7b89556168fb3c09b3392a689
zhaoxiemn <qddarrenxie@gmail.com>
2014-11-06 15:06:22 -0600

    changed

--- PizzaAnonymousSystem/PizzaModels/Models/Provider.cs
+++ PizzaAnonymousSystem/PizzaModels/Models/Provider.cs
@@ -8,14 +8,8 @@ namespace PizzaModels.Models
     public class Provider
     {
         private long _bankAccount;
-        public Provider()
-        {
-            _bankAccount = 0;
-        }
-        public Provider(long num)
-        {
-            _bankAccount = num;
-        }
+        public Provider(){}
+        public Provider(long num){_bankAccount = num;}
         public long BankAccount{get {return _bankAccount;} set {_bankAccount = value;}}
     }
 }
