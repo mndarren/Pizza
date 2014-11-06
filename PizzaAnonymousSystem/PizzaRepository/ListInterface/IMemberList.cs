3e118143c20408304b279e6309684a0bc5723b73
leonluocheng <leon.luocheng@gmail.com>
2014-11-06 16:38:45 -0600

    NewUpdate

--- PizzaAnonymousSystem/PizzaRepository/ListInterface/IMemberList.cs
+++ PizzaAnonymousSystem/PizzaRepository/ListInterface/IMemberList.cs
@@ -3,11 +3,15 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using PizzaModels.Models;
 
 namespace PizzaRepository.ListInterface
 {
     public interface IMemberList
     {
-        
+        Boolean InsertMember(Member member);
+        List<Member> GetMembers();
+        Boolean DeleteMember(int memberID);
+        Boolean UpdateMember(int memberID, int _status);
     }
 }
