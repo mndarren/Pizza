5cdd0d8da530d5e92385e2f458a5414b60ad8bc3
zhaoxiemn <qddarrenxie@gmail.com>
2014-11-05 22:17:02 -0600

    singleton

--- PizzaAnonymousSystem/PizzaRepository/ListClass/ProviderDirectory.cs
+++ PizzaAnonymousSystem/PizzaRepository/ListClass/ProviderDirectory.cs
@@ -3,11 +3,14 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using PizzaModels.Models;
+using PizzaRepository.ListInterface;
 
 namespace PizzaRepository.ListClass
 {
     public class ProviderDirectory : IProviderDirectory
     {
+        private List<Service> serviceItems;
 
     }
 }
