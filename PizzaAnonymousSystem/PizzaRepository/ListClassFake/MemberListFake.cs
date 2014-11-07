add5a48898232bf8882c2977183912d89648cf94
leonluocheng <leon.luocheng@gmail.com>
2014-11-06 21:35:53 -0600

    NewUpdate

--- PizzaAnonymousSystem/PizzaRepository/ListClassFake/MemberListFake.cs
+++ PizzaAnonymousSystem/PizzaRepository/ListClassFake/MemberListFake.cs
@@ -3,7 +3,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
-using PizzaModels.Models
+using PizzaModels.Models;
 
 namespace PizzaRepository.ListClassFake
 {
