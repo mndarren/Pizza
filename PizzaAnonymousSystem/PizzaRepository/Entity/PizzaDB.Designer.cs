1e2ee08d3c245ef93713e9ab804241361a4b1631
panshengti <panshengti@hotmail.com>
2014-11-16 19:51:16 -0600

    provider interface

--- PizzaAnonymousSystem/PizzaRepository/Entity/PizzaDB.Designer.cs
+++ PizzaAnonymousSystem/PizzaRepository/Entity/PizzaDB.Designer.cs
@@ -1,4 +1,4 @@
-﻿// T4 code generation is enabled for model 'C:\Users\Zhao\Documents\GitHub\Pizza\PizzaAnonymousSystem\PizzaRepository\Entity\PizzaDB.edmx'. 
+﻿// T4 code generation is enabled for model 'C:\Users\Shengti Pan\Documents\GitHub\Pizza\PizzaAnonymousSystem\PizzaRepository\Entity\PizzaDB.edmx'. 
 // To enable legacy code generation, change the value of the 'Code Generation Strategy' designer
 // property to 'Legacy ObjectContext'. This property is available in the Properties Window when the model
 // is open in the designer.
