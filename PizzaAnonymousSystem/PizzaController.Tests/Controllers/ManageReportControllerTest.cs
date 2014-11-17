115b3594cfc7be56cb174e3a7c37c8eee1b15285
panshengti <panshengti@hotmail.com>
2014-11-16 20:55:48 -0600

    update

--- PizzaAnonymousSystem/PizzaController.Tests/Controllers/ManageReportControllerTest.cs
+++ PizzaAnonymousSystem/PizzaController.Tests/Controllers/ManageReportControllerTest.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using PizzaRepository.ListClassFake;
 using PizzaController.Controllers;
+using PizzaModels.Report;
+
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 
 namespace PizzaController.Tests.Controllers
@@ -13,8 +15,8 @@ namespace PizzaController.Tests.Controllers
         public void TestMemberReport()
         {
             var report = new ManageReportController(new MemberListFake(), new ProviderListFake(), new ProviderDirectoryFake(), new ScheduleListFake(), new ServiceRecordListFake());
-            //var result = report.GetWeeklyMemberReports();
-           // Assert.IsFalse(null != result);
+            var result = report.GetWeeklyMemberReports();
+            Assert.IsFalse(null != result);
         }
     }
 }
