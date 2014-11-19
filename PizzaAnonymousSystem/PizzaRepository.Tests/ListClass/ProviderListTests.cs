2e789659389e88bc014643cfe9bc25d6f8c568b7
zhaoxiemn <qddarrenxie@gmail.com>
2014-11-18 20:57:46 -0600

    updated

--- PizzaAnonymousSystem/PizzaRepository.Tests/ListClass/ProviderListTests.cs
+++ PizzaAnonymousSystem/PizzaRepository.Tests/ListClass/ProviderListTests.cs
@@ -16,8 +16,8 @@ namespace PizzaRepository.Tests.ListClass
 
             var newProvider = new Provider("Zhao Xie", "397 4th Ave S", "MN", "St. Cloud", "56301",100000023434);
             var success = repository.AddProvider(newProvider);
-            var providerList = repository.GetAllProviders();
 
+            var providerList = repository.GetAllProviders();
             var provider = providerList[providerList.Count - 1];
 
             Assert.IsTrue(success, "addition fail");
@@ -29,29 +29,46 @@ namespace PizzaRepository.Tests.ListClass
             Assert.AreEqual(newProvider.City,provider.City,"cities are not equal");
             Assert.AreEqual(newProvider.BankAccount, provider.BankAccount, "bankaccounts are not equal");
 
-            success = repository.DeleteProvider(provider.ID);
-            Assert.IsTrue(success, "delete fail");
         }
 
         [TestMethod]
         [TestCategory("ProviderList")]
         public void GetProvider()
         {
-            Assert.Fail("not implemented yet");
+            var repository = new ProviderList();
+            var provider = repository.GetProvider(102);
+
+            Assert.IsTrue(null != provider, "returned provider does not exist");
         }
 
         [TestMethod]
         [TestCategory("ProviderList")]
         public void UpdateProvider()
         {
-            Assert.Fail("not implemented yet");
+            var repository = new ProviderList();
+            var providerList = repository.GetAllProviders();
+            var provider = providerList[providerList.Count - 1];
+            var updatedProvider = repository.UpdateProvider("Mo", 102, "100th Ave", "city", "MM", "30093", 2341341);
+
+            Assert.IsTrue(updatedProvider != null, "returned updated provider");
+
+            Assert.AreEqual(updatedProvider.Name, provider.Name, "names are not equal");
+            Assert.AreEqual(updatedProvider.StreetAddress, provider.StreetAddress, "addresses are not equal");
+            Assert.AreEqual(updatedProvider.State, provider.State, "states are not equal");
+            Assert.AreEqual(updatedProvider.City, provider.City, "cities are not equal");
+            Assert.AreEqual(updatedProvider.BankAccount, provider.BankAccount, "bankaccounts are not equal");
         }
 
         [TestMethod]
         [TestCategory("ProviderList")]
         public void DeleteProvider()
         {
-            Assert.Fail("not implemented yet");
+            var repository = new ProviderList();
+            var providerList = repository.GetAllProviders();
+            var provider = providerList[providerList.Count - 1];
+
+            var success = repository.DeleteProvider(provider.ID);
+            Assert.IsTrue(success, "delete fail");
         }
     }
 }
