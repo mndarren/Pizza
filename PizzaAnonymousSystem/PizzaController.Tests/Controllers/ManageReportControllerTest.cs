0fd49b438f221e956c091d3b57c528535ae1a570
panshengti <panshengti@hotmail.com>
2014-11-18 20:36:31 -0600

    update manager

--- PizzaAnonymousSystem/PizzaController.Tests/Controllers/ManageReportControllerTest.cs
+++ PizzaAnonymousSystem/PizzaController.Tests/Controllers/ManageReportControllerTest.cs
@@ -35,6 +35,36 @@ namespace PizzaController.Tests.Controllers
             var report = new ManageReportController(new MemberList(), new ProviderList(), new ProviderDirectory(), new ScheduleList(), new ServiceRecordList());
             report.GetWeeklyEFTReports();
         }
+
+        [TestMethod]
+        [TestCategory("ManageReportController")]
+        public void TestUpdateMemberReportSchedule()
+        {
+            var report = new ManageReportController(new MemberList(), new ProviderList(), new ProviderDirectory(), new ScheduleList(), new ServiceRecordList());
+            TimeSpan ts = new TimeSpan(2, 14, 0);
+            report.UpdateMemberReportSchedule(2,ts);
+        }
+
+
+        [TestMethod]
+        [TestCategory("ManageReportController")]
+        public void TestUpdateProviderReportSchedule()
+        {
+            var report = new ManageReportController(new MemberList(), new ProviderList(), new ProviderDirectory(), new ScheduleList(), new ServiceRecordList());
+            TimeSpan ts = new TimeSpan(2, 14, 0);
+            report.UpdateProviderReportSchedule(2, ts);
+        }
+
+        [TestMethod]
+        [TestCategory("ManageReportController")]
+        public void TestUpdateEFTReportSchedule()
+        {
+            var report = new ManageReportController(new MemberList(), new ProviderList(), new ProviderDirectory(), new ScheduleList(), new ServiceRecordList());
+            TimeSpan ts = new TimeSpan(2, 14, 0);
+            report.UpdateEFTReportSchedule(2, ts);
+        }
+
+
     }
 
 
