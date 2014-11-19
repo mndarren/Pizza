46f373afb709b1707803f4989d4d4c9e912bcf62
zhaoxiemn <qddarrenxie@gmail.com>
2014-11-18 19:12:48 -0600

    updated

--- PizzaAnonymousSystem/PizzaRepository.Tests/ListClass/ProviderListTests.cs
+++ PizzaAnonymousSystem/PizzaRepository.Tests/ListClass/ProviderListTests.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using 
 
 namespace PizzaRepository.Tests.ListClass
 {
@@ -10,7 +11,22 @@ namespace PizzaRepository.Tests.ListClass
         [TestCategory("ProviderList")]
         public void AddProvider()
         {
-            Assert.Fail("not implemented yet");
+            var repository = new ProviderList();
+            var testReportType = 99;
+
+            var newSchedule = new Schedule(testReportType, 5, new TimeSpan(12, 59, 59));
+            var success = repository.AddSchedule(newSchedule);
+            var schedule = repository.GetSchedule(testReportType);
+
+            Assert.IsTrue(success, "addition fail");
+            Assert.IsTrue(null != schedule, "returned schedule does not exist");
+
+            Assert.AreEqual(newSchedule.ReportType, schedule.ReportType, "report types are not equal");
+            Assert.AreEqual(newSchedule.Time, schedule.Time, "times are not equal");
+            Assert.AreEqual(newSchedule.Week, schedule.Week, "weeks are not equal");
+
+            success = repository.DeleteSchedule(testReportType);
+            Assert.IsTrue(success, "delete fail");
         }
 
         [TestMethod]
