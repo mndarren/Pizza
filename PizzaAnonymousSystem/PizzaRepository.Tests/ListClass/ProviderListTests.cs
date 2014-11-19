e4f730cb5b2987ceac0e69682053a3c692251469
leonluocheng <leon.luocheng@gmail.com>
2014-11-18 20:54:45 -0600

    MyChange

--- PizzaAnonymousSystem/PizzaRepository.Tests/ListClass/ProviderListTests.cs
+++ PizzaAnonymousSystem/PizzaRepository.Tests/ListClass/ProviderListTests.cs
@@ -12,21 +12,6 @@ namespace PizzaRepository.Tests.ListClass
         [TestCategory("ProviderList")]
         public void AddProvider()
         {
-            var repository = new ProviderList();
-
-            var newProvider = new Provider("Zhao Xie", "397 4th Ave S", "MN", "St. Cloud", "56301",100000023434);
-            var success = repository.AddProvider(newProvider);
-            var schedule = repository.GetProvider(testReportType);
-
-            Assert.IsTrue(success, "addition fail");
-            Assert.IsTrue(null != schedule, "returned schedule does not exist");
-
-            Assert.AreEqual(newSchedule.ReportType, schedule.ReportType, "report types are not equal");
-            Assert.AreEqual(newSchedule.Time, schedule.Time, "times are not equal");
-            Assert.AreEqual(newSchedule.Week, schedule.Week, "weeks are not equal");
-
-            success = repository.DeleteSchedule(testReportType);
-            Assert.IsTrue(success, "delete fail");
         }
 
         [TestMethod]
