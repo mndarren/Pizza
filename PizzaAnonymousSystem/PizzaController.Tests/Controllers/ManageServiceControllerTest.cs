7ca0b679bd76e136d55589ec935a627c3b0a1a52
zhaoxiemn <qddarrenxie@gmail.com>
2014-11-18 22:06:41 -0600

    updated

--- PizzaAnonymousSystem/PizzaController.Tests/Controllers/ManageServiceControllerTest.cs
+++ PizzaAnonymousSystem/PizzaController.Tests/Controllers/ManageServiceControllerTest.cs
@@ -15,9 +15,31 @@ namespace PizzaController.Tests.Controllers
         public void TestAddServiceRecord()
         {
             var report = new ManageServiceController(new MemberList(),new ProviderList(),new ProviderDirectory(), new ServiceRecordList());
-            DateTime timeEx = new DateTime();
             var result = report.AddServiceRecord(new ServiceRecord(123456,DateTime.Now, DateTime.Today, 10005,2009, "NoComment"));
-
+            Assert.IsTrue(result, "Failed to add a new service record.");
+        }
+        [TestMethod]
+        [TestCategory("ManageServiceController")]
+        public void TestAddService()
+        {
+            var report = new ManageServiceController(new MemberList(), new ProviderList(), new ProviderDirectory(), new ServiceRecordList());
+            var result = report.AddService(new Service(222233, "handHealth", 150m));
+            Assert.IsTrue(result, "Failed to add a new service");
+        }
+        [TestMethod]
+        [TestCategory("ManageServiceController")]
+        public void TestUpdateService()
+        {
+            var report = new ManageServiceController(new MemberList(), new ProviderList(), new ProviderDirectory(), new ServiceRecordList());
+            var newService = new Service(222233,"Wash hair",45m);
+            var updatedService = report.UpdateService(newService);
+            Assert.IsTrue(updatedService != null, "Failed to update a service");
+        }
+        [TestMethod]
+        [TestCategory("ManageServiceController")]
+        public void TestUpdateService() 
+        {
+           
         }
     }
 }
