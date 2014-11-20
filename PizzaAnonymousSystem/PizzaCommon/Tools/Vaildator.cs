c399fa408effa5e96a2a8c350357897c61502683
leonluocheng <leon.luocheng@gmail.com>
2014-11-20 17:27:58 -0600

    Change

--- PizzaAnonymousSystem/PizzaCommon/Tools/Vaildator.cs
+++ PizzaAnonymousSystem/PizzaCommon/Tools/Vaildator.cs
@@ -10,12 +10,12 @@ namespace PizzaCommon.Tools
     {
         private const string checkingName = @"/^[a-zA-ZàáâäãåąčćęèéêëėįìíîïłńòóôöõøùúûüųūÿýżźñçčšžÀÁÂÄÃÅĄĆČĖĘÈÉÊËÌÍÎÏĮŁŃÒÓÔÖÕØÙÚÛÜŲŪŸÝŻŹÑßÇŒÆČŠŽ∂ð ,.'-]+$/u";
         private const string checkingAddress = @"\d{1,3}.?\d{0,3}\s[a-zA-Z]{2,30}\s[a-zA-Z]{2,15}";
-        private const string checkingCity = @"";
-        private const string checkingState = @"";
-        private const string checkingZIPCode = @"";
+        private const string checkingCity = @"^[a-zA-Z]+(?:[\s-][a-zA-Z]+)*$";
+        private const string checkingState = @"^(?-i:A[LKSZRAEP]|C[AOT]|D[EC]|F[LM]|G[AU]|HI|I[ADLN]|K[SY]|LA|M[ADEHINOPST]|N[CDEHJMVY]|O[HKR]|P[ARW]|RI|S[CD]|T[NX]|UT|V[AIT]|W[AIVY])$";
+        private const string checkingZIPCode = @"^\d{5}(?:[-\s]\d{4})?$";
         private const string checkingBankAccount = @"";
-        private const int checkingServiceCode;
-        private const int checkingFee;
+        private int checkingServiceCode;
+        private int checkingFee;
 
         public string CheckingMember() { }
         public string CheckingProvider() { }
