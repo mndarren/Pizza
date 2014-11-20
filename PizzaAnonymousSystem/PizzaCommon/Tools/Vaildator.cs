a499f07c8e361258c882ecd50ab215b1a5c6477a
leonluocheng <leon.luocheng@gmail.com>
2014-11-20 16:52:52 -0600

    NewChange

---
+++ PizzaAnonymousSystem/PizzaCommon/Tools/Vaildator.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PizzaCommon.Tools
+{
+    public static class Vaildator
+    {
+        private string checkingName = @"/^[a-zA-ZàáâäãåąčćęèéêëėįìíîïłńòóôöõøùúûüųūÿýżźñçčšžÀÁÂÄÃÅĄĆČĖĘÈÉÊËÌÍÎÏĮŁŃÒÓÔÖÕØÙÚÛÜŲŪŸÝŻŹÑßÇŒÆČŠŽ∂ð ,.'-]+$/u";
+        private string checkingAddress = @"";
+        private string checkingCity = @"";
+        private string checkingState = @"";
+        private string checkingZIPCode = @"";
+        private string checkingBankAccount = @"";
+        private int checkingServiceCode;
+        private int checkingFee;
+
+        public string CheckingMember() { }
+        public string CheckingProvider() { }
+        public string CheckingManager() { }
+        public string CheckingAdmin() { }
+        public string CheckingSchedule() { }
+        public string CheckingService() { }
+        public string CheckingServiceReocr() { }
+
+    }
+}
