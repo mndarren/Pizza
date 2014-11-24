b504b3c785bac4e805e136bf92aa156d930559c6
zhaoxiemn <qddarrenxie@gmail.com>
2014-11-23 22:42:31 -0600

    update get provider and service

--- PizzaAnonymousSystem/PizzaGUI/Scripts/Tools/Service/GetAllServices.js
+++ PizzaAnonymousSystem/PizzaGUI/Scripts/Tools/Service/GetAllServices.js
@@ -1,1 +1,36 @@
 ﻿
+$(document).ready(function () {
+    document.getElementById("get-service-reset").onclick = getServiceReset;
+    document.getElementById("get-service-submit").onclick = getServiceSubmit;
+
+    function getServiceReset() {
+        event.preventDefault();
+        $('#get-service-code').val("");
+    }
+
+    function getServiceSubmit() {
+        event.preventDefault();
+
+        $.ajax({
+            type: 'get',
+            crossDomain: true,
+            url: 'http://localhost:49890/api/servicemanager/services/' + '/' + $('#get-service-code').val(),
+            contentType: 'application/json; charset=utf-8',
+            dataType: "json",
+            beforeSend: function (xhr) {
+                xhr.setRequestHeader("Access-Control-Allow-Origin", "*");
+                $('#get-service-loader').removeClass("visibility-hidden");
+            },
+            success: function (data) {
+                $('#get-service-success').slideToggle(400).delay(3000).slideToggle(400);
+                getServiceReset();
+            },
+            error: function (error) {
+                $('#get-service-error').slideToggle(400).delay(3000).slideToggle(400);
+            },
+            complete: function () {
+                $('#get-service-loader').addClass("visibility-hidden");
+            }
+        });
+    }
+});
