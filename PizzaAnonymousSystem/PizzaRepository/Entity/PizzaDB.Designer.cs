47ea6288b29a012f5c03d728e14dbc50f0d3c3bb
leonluocheng <leon.luocheng@gmail.com>
2014-11-23 21:51:14 -0600

    Mychange

--- PizzaAnonymousSystem/PizzaRepository/Entity/PizzaDB.Designer.cs
+++
@@ -1,4 +0,0 @@
-﻿// Default code generation is disabled for model 'C:\Users\Edward\Documents\GitHub\Pizza\PizzaAnonymousSystem\PizzaRepository\Entity\PizzaDB.edmx'. 
-// To enable default code generation, change the value of the 'Code Generation Strategy' designer
-// property to an alternate value. This property is available in the Properties Window when the model is
-// open in the designer.
