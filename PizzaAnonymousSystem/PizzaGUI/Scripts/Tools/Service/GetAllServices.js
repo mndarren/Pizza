01b423af4497c50b034225eb5680e0a63344dbd5
edwardiscsu <ised03001@stcloudstate.edu>
2014-11-25 23:06:36 -0600

    Reverted the DB

--- PizzaAnonymousSystem/PizzaGUI/Scripts/Tools/Service/GetAllServices.js
+++ PizzaAnonymousSystem/PizzaGUI/Scripts/Tools/Service/GetAllServices.js
@@ -1,36 +1,37 @@
 ﻿
 $(document).ready(function () {
-    //document.getElementById("get-service-reset").onclick = getServiceReset;
-    //document.getElementById("get-service-submit").onclick = getServiceSubmit;
-    //
-    //function getServiceReset() {
-    //    event.preventDefault();
-    //    $('#get-service-code').val("");
-    //}
-    //
-    //function getServiceSubmit() {
-    //    event.preventDefault();
-    //
-    //    $.ajax({
-    //        type: 'get',
-    //        crossDomain: true,
-    //        url: 'http://localhost:49890/api/servicemanager/services/' + '/' + $('#get-service-code').val(),
-    //        contentType: 'application/json; charset=utf-8',
-    //        dataType: "json",
-    //        beforeSend: function (xhr) {
-    //            xhr.setRequestHeader("Access-Control-Allow-Origin", "*");
-    //            $('#get-service-loader').removeClass("visibility-hidden");
-    //        },
-    //        success: function (data) {
-    //            $('#get-service-success').slideToggle(400).delay(3000).slideToggle(400);
-    //            getServiceReset();
-    //        },
-    //        error: function (error) {
-    //            $('#get-service-error').slideToggle(400).delay(3000).slideToggle(400);
-    //        },
-    //        complete: function () {
-    //            $('#get-service-loader').addClass("visibility-hidden");
-    //        }
-    //    });
-    //}
+
+    function AllServicesViewModel() {
+        var self = this;
+
+        self.services = ko.observableArray([]);
+
+        self.refreshServices = function () {
+            $.ajax({
+                type: 'POST',
+                crossDomain: true,
+                url: 'http://localhost:49890/api/servicemanager/get/services',
+                contentType: 'application/json; charset=utf-8',
+                dataType: "json",
+                beforeSend: function (xhr) {
+                    xhr.setRequestHeader("Access-Control-Allow-Origin", "*");
+                    $('#get-member-loader').removeClass("visibility-hidden");
+                },
+                success: function (data) {
+                    $('#get-member-success').slideToggle(400).delay(3000).slideToggle(400);
+                    
+
+                },
+                error: function (error) {
+                    $('#get-member-error').slideToggle(400).delay(3000).slideToggle(400);
+                },
+                complete: function () {
+                    $('#get-member-loader').addClass("visibility-hidden");
+                }
+            });
+        };
+    };
+
+    ko.applyBindings(new AllServicesViewModel());
+
 });
