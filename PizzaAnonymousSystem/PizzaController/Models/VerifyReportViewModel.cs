bf046a62e003faef80e1804dbcacf5921bbb5ad1
edwardiscsu <ised03001@stcloudstate.edu>
2014-11-30 20:39:12 -0600

    implement verify fees

--- PizzaAnonymousSystem/PizzaController/Models/VerifyReportViewModel.cs
+++ PizzaAnonymousSystem/PizzaController/Models/VerifyReportViewModel.cs
@@ -9,14 +9,14 @@ namespace PizzaController.Models
     {
         #region Private Members
 
-        private int _providerID;
+        private int _providerNumber;
 
         private DateTime _startDate;
         private DateTime _endDate;
 
         #endregion
 
-        public int ProviderID { get { return _providerID; } set { _providerID = value; } }
+        public int ProviderNumber { get { return _providerNumber; } set { _providerNumber = value; } }
 
         public DateTime StartDate { get { return _startDate; } set { _startDate = value; } }
         public DateTime EndDate { get { return _endDate; } set { _endDate = value; } }
@@ -26,7 +26,7 @@ namespace PizzaController.Models
 
         public VerifyReportViewModel(int providerID, DateTime startDate, DateTime endDate)
         {
-            this.ProviderID = providerID;
+            this.ProviderNumber = providerID;
             this.StartDate = startDate;
             this.EndDate = endDate;
         }
